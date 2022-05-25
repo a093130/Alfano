f748ce3dc96e343eb39e860d94d4c1950577cb88
Colin <colinhelms@outlook.com>
2022-05-25 14:46:59 -0700

    Rename src directory to alfano so that distribution has correct module name

--- setup.py
+++ setup.py
@@ -8,20 +8,20 @@ Created on Sun Apr 28 15:08:37 2019
 
 from distutils.core import setup
 setup(name='alfano',
-      version='0.5a1',
+      version='0.5b1',
       description='Edelbaum-Alfano Low-Thrust Trajectories.',
       long_description='file: README.txt',
       author='Colin Helms',
       author_email='colinhelms@outlook.com',
       url='https://github.com/a093130/Alfano',
-      packages=['', 'src', 'src.utilities', 'src.controls'],
+      packages=['', 'alfano', 'alfano.utilities', 'alfano.controls'],
       classifiers=[
               'Development Status :: Alpha',
               'Environment :: Console',
               'Operating System :: Microsoft :: Windows 10',
               'Operating System :: Linux :: Ubuntu 18.4',
-              'Programming Language :: Python :: 3.4',
-              'License :: End User License Agreement',
-              'Topic :: Copyright :: Copyright Freelance Rocket Science, 2022'
-              ],
+              'Programming Language :: Python :: 3.7',
+              'License :: GNU General Public License v3 (GPLv3)',
+              'Topic :: Copyright :: Copyright Freelance Rocket Science, 2018 - 2022'
+              ]
         )
